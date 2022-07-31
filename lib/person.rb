ffac414771cfe0deaff5ee2ccac556f1b662901e
majaliju <majalijunyc@gmail.com>
2022-07-30 22:07:53 -0400

    updating but not done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,30 @@
-# your code goes here
+class Person
+  attr_reader :name, :hygiene, :happiness
+  attr_accessor :bank_account
+
+  # setter for @happiness
+  def happiness
+    @happiness = happiness.clamp(0, 10)
+  end
+  
+  # setter for @hygiene
+  def hygiene
+    @hygiene = hygiene.clamp(0, 10)
+  end
+
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def clean? 
+    if @hygiene > 7 
+      true
+    else false 
+    end
+  end
+
+end
