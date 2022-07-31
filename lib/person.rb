49161d0d905aed5c40ca7ecd612803084f8e1c98
majaliju <majalijunyc@gmail.com>
2022-07-30 22:28:04 -0400

    updated

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Person
   attr_reader :name
   attr_writer :hygiene, :happiness
@@ -37,21 +39,40 @@ class Person
     end
   end
 
-  def get_paid amount
-    bank_account += amount
+  def get_paid(amount)
+    self.bank_account += amount
     "all about the benjamins"
   end
 
   def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    '♪ another one bites the dust ♫'
   end
 
-  def call_friend
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
-  def start_conversation (person, topic)
+  def start_conversation (friend, topic)
+    if topic == 'politics'
+      friend.happiness -= 2
+      self.happiness -= 2
+      'blah blah partisan blah lobbyist'
+    elsif topic == 'weather'
+      friend.happiness += 1
+      self.happiness += 1
+      'blah blah sun blah rain'
+    else 
+      'blah blah blah blah blah'
+    end
   end
   
 end
