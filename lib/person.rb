fa505b94e29ac14a93113684183d567b6d7e77cd
majaliju <majalijunyc@gmail.com>
2022-07-30 22:09:18 -0400

    updating but not done

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,16 @@
 class Person
-  attr_reader :name, :hygiene, :happiness
-  attr_accessor :bank_account
+  attr_reader :name
+  attr_accessor :bank_account, :hygiene, :happiness
 
-  # setter for @happiness
-  def happiness
-    @happiness = happiness.clamp(0, 10)
-  end
+  # # setter for @happiness
+  # def happiness
+  #   @happiness = happiness.clamp(0, 10)
+  # end
   
-  # setter for @hygiene
-  def hygiene
-    @hygiene = hygiene.clamp(0, 10)
-  end
+  # # setter for @hygiene
+  # def hygiene
+  #   @hygiene = hygiene.clamp(0, 10)
+  # end
 
 
   def initialize(name)
